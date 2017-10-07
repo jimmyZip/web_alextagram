cbd48301d6900d89f09c93f1e8455b72a3dc9730
Alex Kyungho Park <kyungho96@gmail.com>
2017-10-08 00:37:51 +0900

    requirements

--- vietgram/settings.py
+++ vietgram/settings.py
@@ -50,7 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+#MIDDLEWARE_CLASSES = (
+#    'whitenoise.middleware.WhiteNoiseMiddleware'
+#)
 
 ROOT_URLCONF = 'vietgram.urls'
 
@@ -127,6 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'users.User'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
